1123548989f871676e926b926b5a70e8257ea23b
Kris Jenkins <krisajenkins@gmail.com>
2014-01-21 09:35:02 +0000

    Adding timeline reads.

--- src/valtweet/core.clj
+++ src/valtweet/core.clj
@@ -1,11 +1,21 @@
-(ns valtweet.core)
-
-(defrecord MemoryTweetStore [])
+(ns valtweet.core
+  (:require [clj-time.core :refer [now]]))
 
 (defprotocol TweetStore
-  (post [store username text]))
+  (post [store username text time])
+  (tweets-by [store username]))
 
-(extend-type MemoryTweetStore
+(extend-type clojure.lang.PersistentHashSet
   TweetStore
-  (post [store username text]
-    store))
+  (post [store username text time]
+    (conj store
+          {:username username
+           :text text
+           :time time}))
+  (tweets-by [store username]
+    (->> store
+         (filter (fn [tweet]
+                   (= username
+                      (:username tweet))))
+         (sort-by :time)
+         reverse)))
--- test/valtweet/core_test.clj
+++ test/valtweet/core_test.clj
@@ -1,10 +1,16 @@
 (ns valtweet.core-test
   (:require [clojure.test :refer :all]
-            [valtweet.core :refer :all]))
+            [valtweet.core :refer :all]
+            [clj-time.core :refer [minus now minutes]]))
 
-(deftest a-test
-  (testing "Posting"
-    (-> (->MemoryTweetStore)
-        (post "Alice" "I love the weather today")
-        (post "Bob" "Damn! We lost!")
-        (post "Bob" "Good game though."))))
+(deftest timeline-tests
+  (testing "Post and read"
+    (let [store (-> #{}
+                    (post "Alice" "I love the weather today" (minus (now) (minutes 5)))
+                    (post "Bob" "Damn! We lost!"             (minus (now) (minutes 2)))
+                    (post "Bob" "Good game though."          (minus (now) (minutes 1))))]
+      (is (= (map :text (tweets-by store "Alice"))
+             ["I love the weather today"]))
+      (is (= (map :text (tweets-by store "Bob"))
+             ["Good game though."
+              "Damn! We lost!"])))))
