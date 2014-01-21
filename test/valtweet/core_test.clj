d59119ddb2dda0a5cae2b2fd202012e3bfecd8f7
Kris Jenkins <krisajenkins@gmail.com>
2014-01-21 10:16:30 +0000

    Reworking the TweetStore to take the tweet as a record.

--- test/valtweet/core_test.clj
+++ test/valtweet/core_test.clj
@@ -6,9 +6,9 @@
 (deftest timeline-tests
   (testing "Post and read"
     (let [store (-> #{}
-                    (post "Alice" "I love the weather today" (minus (now) (minutes 5)))
-                    (post "Bob" "Damn! We lost!"             (minus (now) (minutes 2)))
-                    (post "Bob" "Good game though."          (minus (now) (minutes 1))))]
+                    (post (->Tweet "Alice" "I love the weather today" (minus (now) (minutes 5))))
+                    (post (->Tweet "Bob"   "Damn! We lost!"           (minus (now) (minutes 2))))
+                    (post (->Tweet "Bob"   "Good game though."        (minus (now) (minutes 1)))))]
       (is (= (map :text (tweets-by store "Alice"))
              ["I love the weather today"]))
       (is (= (map :text (tweets-by store "Bob"))
