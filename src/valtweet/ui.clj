eed32ec34e0edbb2ccef62c6ef7c09057d7afdfe
Kris Jenkins <krisajenkins@gmail.com>
2014-01-21 14:09:08 +0000

    Adding a command parser.

--- src/valtweet/ui.clj
+++ src/valtweet/ui.clj
@@ -1,5 +1,7 @@
 (ns valtweet.ui
-  (:require [valtweet.util :refer [humanize-date]]))
+  (:require [valtweet.util :refer [humanize-date]]
+            [clojure.java.io :refer [resource]]
+            [instaparse.core :as instaparse]))
 
 (defn format-tweet
   [{:keys [username text time]}]
@@ -7,3 +9,29 @@
           username
           text
           (humanize-date time)))
+
+(def parser
+  (instaparse/parser (resource "valtweet/commands.bnf")))
+
+(defn parse-command
+  [string]
+
+  (->> (parser string)
+       (instaparse/transform {:username identity
+                              :freetext identity
+                              :command identity
+                              :follow-command (fn [username _ _ _ followers-username]
+                                                {:command :follow
+                                                 :username username
+                                                 :follows-username followers-username})
+                              :read-command (fn [username]
+                                                {:command :read
+                                                 :username username})
+                              :tweet-command (fn [username _ _ _ text]
+                                                {:command :post
+                                                 :username username
+                                                 :text text})
+                              :wall-command (fn [username _ _]
+                                                {:command :wall
+                                                 :username username})
+                              })))
--- test/valtweet/ui_test.clj
+++ test/valtweet/ui_test.clj
@@ -15,3 +15,32 @@
 (deftest format-tweet-test
   (is (= (format-tweet (->Tweet "Alice" "I love the weather today" (minus (now) (minutes 5))))
          "Alice - I love the weather today (5 minutes ago)")))
+
+(deftest parse-command-test
+  (are [command-string result] (= (parse-command command-string)
+                                  result)
+
+       "Alice -> Nice day." {:command :post
+                             :username "Alice"
+                             :text "Nice day."}
+       "Bob -> Says you."   {:command :post
+                             :username "Bob"
+                             :text "Says you."}
+
+       "Alice"              {:command :read
+                             :username "Alice"}
+       "Bob"                {:command :read
+                             :username "Bob"}
+
+       "Alice follows Bob"  {:command :follow
+                             :username "Alice"
+                             :follows-username "Bob"}
+       "Bob follows Steve"  {:command :follow
+                             :username "Bob"
+                             :follows-username "Steve"}
+
+       "Alice wall"         {:command :wall
+                             :username "Alice"}
+
+       "Bob wall"           {:command :wall
+                             :username "Bob"}))
