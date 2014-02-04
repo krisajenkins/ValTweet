4f6ae68244931fa6d348683ee65d6e355b096959
Kris Jenkins <krisajenkins@gmail.com>
2014-02-04 16:41:28 +0000

    Adding lein-midje as a dev dependency.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
                  [midje "1.6.0"]
                  [clj-time "0.6.0"]
                  [instaparse "1.2.14"]]
+  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}}
   :main valtweet.ui)
